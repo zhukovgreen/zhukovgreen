458ce9ccd570c8133f884a77457b897887c4bcbf
ZhukovGreen <zhukovgreen@icloud.com>
2020-02-19 15:58:09 +0100

    Ensure the file will be created or updated

--- scripts/commit_updated_files.py
+++ scripts/commit_updated_files.py
@@ -5,9 +5,10 @@ import sys
 from envparse import env
 from git import Repo
 from github import Github
+from github.GithubException import UnknownObjectException
 from github.Repository import Repository
 
-logging.basicConfig(stream=sys.stdout)
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 logger = logging.getLogger(__file__)
 
 
@@ -19,12 +20,18 @@ local_repo = Repo(ROOT_PATH)
 g = Github(login_or_token=env.str("GITHUB_TOKEN"))
 logger.info("Github authenticated")
 
-
 repo: Repository = g.get_repo("ZhukovGreen/CV")
 logger.info("Repository loaded")
 for pdf in (f for f in local_repo.untracked_files if f[-3:] == "pdf"):
-    response = repo.create_file(
+    try:
+        contents_sha = repo.get_contents(
+            pdf, ref=local_repo.active_branch.name
+        ).sha
+    except UnknownObjectException:
+        contents_sha = ""
+    response = repo.update_file(
         path=pdf,
+        sha=contents_sha,
         content=(ROOT_PATH / pdf).read_bytes(),
         message=f"Added {pdf}",
         branch=local_repo.active_branch.name,
