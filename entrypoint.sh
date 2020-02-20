3c4c00953caa25adaf1ed9d393324150be038497
zhukovgreen <iam@zhukovgreen.pro>
2020-02-20 14:21:52 +0100

    Allow the entrypoint.sh to bypass args

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,28 @@
 set -ex
 
-for FILE in *.md
-do
-   case "$FILE" in
-   *.md)
-     ;;
-   *)
-     >&2 echo "$FILE should have .md ext"; exit 2
-     ;;
-   esac
-   vim -E -s -c "runtime syntax/2html.vim" -c wqa \
-   "$FILE" && weasyprint -s pdfstyle.css \
-   "$FILE.html" "$FILE.pdf"
-   rm "$FILE.html"
-done
+if [ $# -eq 0 ]
+  then
+    for FILE in *.md
+    do
+       case "$FILE" in
+       *.md)
+         ;;
+       *)
+         >&2 echo "$FILE should have .md ext"; exit 2
+         ;;
+       esac
+       vim -E -s \
+       -c "syntax on" \
+       -c "let g:html_no_progres=1" \
+       -c "runtime syntax/2html.vim" \
+       -c wqa "$FILE" && weasyprint -s pdfstyle.css \
+       "$FILE.html" "$FILE.pdf"
+#       rm "$FILE.html"
+    done
 
-cat ./artem_cv.md > README.md
-poetry run python scripts/commit_updated_files.py
-echo "\n\nAll jobs done successfully!\n\n"
+    cat ./artem_cv.md > README.md
+    poetry run python scripts/commit_updated_files.py
+    echo "\n\nAll jobs done successfully!\n\n"
+fi
+
+exec "$@"
