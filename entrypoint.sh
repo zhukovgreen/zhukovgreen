56f191b438caa5f36dbe839128b2b44872ada7d4
zhukovgreen <iam@zhukovgreen.pro>
2020-02-21 13:47:22 +0100

    Simplify entrypoint
    We no longer want to generate pdfs from all *.md
    Also consider *.md new home

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,26 +1,17 @@
 set -ex
 
+CV="./docs/artem_cv.md"
+
 if [ $# -eq 0 ]
   then
-    for FILE in *.md
-    do
-       case "$FILE" in
-       *.md)
-         ;;
-       *)
-         >&2 echo "$FILE should have .md ext"; exit 2
-         ;;
-       esac
-       vim -E -s \
-       -c "syntax on" \
-       -c "let g:html_no_progres=1" \
-       -c "runtime syntax/2html.vim" \
-       -c wqa "$FILE" && weasyprint -s pdfstyle.css \
-       "$FILE.html" "$FILE.pdf"
-       rm "$FILE.html"
-    done
-
-    cat ./artem_cv.md > README.md
+    vim -E -s \
+    -c "syntax on" \
+    -c "let g:html_no_progres=1" \
+    -c "runtime syntax/2html.vim" \
+    -c wqa "$CV" && weasyprint -s pdfstyle.css \
+    "$CV.html" "$CV.pdf"
+    rm "$CV.html"
+    cat "$CV" > "./README.md"
     poetry run python scripts/commit_updated_files.py
     echo "\n\nAll jobs done successfully!\n\n"
 fi
